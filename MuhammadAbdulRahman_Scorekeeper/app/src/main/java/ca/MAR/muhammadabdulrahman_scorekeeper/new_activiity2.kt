ca68d30abba4226b3a30a26bc4f2b3ccd882f73f
Eng-MAR <Drrahman911@hotmail.com>
2022-10-24 18:30:21 -0400

    lab4.1

--- MuhammadAbdulRahman_Scorekeeper/app/src/main/java/ca/MAR/muhammadabdulrahman_scorekeeper/new_activiity2.kt
+++ MuhammadAbdulRahman_Scorekeeper/app/src/main/java/ca/MAR/muhammadabdulrahman_scorekeeper/new_activiity2.kt
@@ -3,50 +3,55 @@ package ca.MAR.muhammadabdulrahman_scorekeeper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioButton
 import android.widget.TextView
 
 class new_activiity2 : AppCompatActivity() {
-
-    var pakistanscoreincrease=43
-    var pakistanscoredecrease=pakistanscoreincrease--
-    var indiascoreincrease=23
-    var indiascoredecrease=indiascoreincrease--
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new2)
 
-        var pakview= findViewById<TextView>(R.id.pview)
+// Calling Radio Buttons
+        val rb1 = findViewById<RadioButton>(R.id.radioButton)
+        val rb2 = findViewById<RadioButton>(R.id.radioButton2)
 
-        var pi = findViewById<Button>(R.id.pakinc)
-        pi.setOnClickListener {
-            pakistanscoreincrease++
-            pakview.setText("Current Run Rate is: " + "$pakistanscoreincrease" + " runs per over(avg)")
+        rb1.setOnClickListener {
+            var pakSc = 43      //Current Score Defined
+            var pakview = findViewById<TextView>(R.id.pview)
+
+            var pi = findViewById<Button>(R.id.pakinc)
+            pi.setOnClickListener {
+
+                pakview.setText("Current Run Rate is: " + pakSc++ + " runs per over(avg)")
+            }
+
+            var pd = findViewById<TextView>(R.id.pakdec)
+            pd.setOnClickListener {
+                pakview.setText("Current Run Rate is: " + pakSc-- + " runs per over(avg)")
+            }
         }
 
-        var pd=findViewById<TextView>(R.id.pakdec)
-        pd.setOnClickListener{
-            pakistanscoredecrease=pakistanscoreincrease--
-            pakview.setText("Current Run Rate is: " + "$pakistanscoredecrease" + " runs per over(avg)")
+        rb2.setOnClickListener {
+            var indSc = 23      //Current Score Defined
+            var indview = findViewById<TextView>(R.id.iview)
+
+            var ii = findViewById<Button>(R.id.indinc)
+            ii.setOnClickListener {
+
+                indview.setText("Current Run Rate is: " + indSc++ + " runs per over(avg)")
+            }
+
+            var id = findViewById<TextView>(R.id.inddec)
+            id.setOnClickListener {
+                indview.setText("Current Run Rate is: " + indSc-- + " runs per over(avg)")
+            }
         }
+// Goto Previous page
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.setTitle("Cricket Scores Are..")
 
-        var indview= findViewById<TextView>(R.id.iview)
-
-        var ii = findViewById<Button>(R.id.indinc)
-        ii.setOnClickListener {
-            indiascoreincrease++
-            indview.setText("Current Run Rate is: " + "$indiascoreincrease" + " runs per over(avg)")
-        }
-
-        var id= findViewById<Button>(R.id.inddec)
-        id.setOnClickListener{
-            indiascoredecrease=indiascoreincrease--
-            indview.setText("Current Run Rate is: " + "$indiascoredecrease" + " runs per over(avg)")
-        }
-
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.setTitle("Cricket Scores Are..")
     }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
--- MuhammadAbdulRahman_Scorekeeper/app/src/main/java/ca/MAR/muhammadabdulrahman_scorekeeper/new_activity1.kt
+++ MuhammadAbdulRahman_Scorekeeper/app/src/main/java/ca/MAR/muhammadabdulrahman_scorekeeper/new_activity1.kt
@@ -13,58 +13,48 @@ import android.widget.Toast
 
 class new_activity1 : AppCompatActivity() {
 
-    var australiascoreincrease = 3
-    var australiascoredecrease = australiascoreincrease--
-    var englandscoreincrease = 2
-    var englandscoredecrease = englandscoreincrease--
-
-
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new1)
+// Calling Radio Buttons
+        val rb3 = findViewById<RadioButton>(R.id.ausradioButton)
+        val rb4 = findViewById<RadioButton>(R.id.engradioButton)
 
+        rb3.setOnClickListener {
+            var ausSc = 3       //Current Score Defined
+            var ausview = findViewById<TextView>(R.id.aview)
 
-        var ausview = findViewById<TextView>(R.id.aview)
+            var ai = findViewById<Button>(R.id.ausinc)
+            ai.setOnClickListener {
 
-        var ai = findViewById<Button>(R.id.ausinc)
-        ai.setOnClickListener {
-            australiascoreincrease++
-            ausview.setText("Current Goal Score is: " + "$australiascoreincrease")
+                ausview.setText("Current Goal Score is: " + ausSc++)  //Score Increase
+            }
+
+            var ad = findViewById<TextView>(R.id.ausdec)
+            ad.setOnClickListener {
+                ausview.setText("Current Goal Score is: " + ausSc--)  //Score Decreased
+            }
         }
+        rb4.setOnClickListener {
+            var engSc = 7       //Current Score Defined
+            var engview = findViewById<TextView>(R.id.eview)
 
-        var ad = findViewById<Button>(R.id.ausdec)
-        ad.setOnClickListener {
-            australiascoredecrease = australiascoreincrease--
-            ausview.setText("Current Goal Score is: " + "$australiascoredecrease")
+            var ei = findViewById<Button>(R.id.enginc)
+            ei.setOnClickListener {
+
+                engview.setText("Current Goal Score is: " + engSc++)  //Score Increase
+            }
+
+            var ed = findViewById<TextView>(R.id.ausdec)
+            ed.setOnClickListener {
+                engview.setText("Current Goal Score is: " + engSc--)  //Score Decreased
+            }
         }
-
-        var engview = findViewById<TextView>(R.id.eview)
-
-        var ei = findViewById<Button>(R.id.enginc)
-        ei.setOnClickListener {
-            englandscoreincrease++
-            engview.setText("Current Goal Score is: " + "$englandscoreincrease")
-        }
-
-        var ed = findViewById<Button>(R.id.engdec)
-        ed.setOnClickListener {
-            englandscoredecrease = englandscoreincrease--
-            engview.setText("Current Goal Score is: " + "$englandscoredecrease")
-        }
-
-        RadioGroup.OnCheckedChangeListener { group, checkedId ->
-            val radio: RadioButton = findViewById(checkedId)
-            Toast.makeText(
-                applicationContext, " On checked change :" +
-                        " ${radio.text}",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-
+//Go To Previous Page
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.setTitle("Football Scores Are..")
+        supportActionBar!!.setTitle("FootBall Scores Are..")
     }
 
     override fun onSupportNavigateUp(): Boolean {
